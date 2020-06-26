58518fb2e2a3cbb3bf859620c3e0b85a048980a0
Jeremy Winn <jeremyjwinn@gmail.com>
2020-06-26 21:34:48 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -32,7 +32,7 @@ def get_japanese_emoticon(file_path, emoticon)
   japanese_meaning = ""
   data.each_with_object({}) do |(key, value)|
     if emoticon == value[:english]
-      japanese_meaning = key
+      japanese_meaning = value[:japanese]
     end
   end
   if japanese_meaning == ""
