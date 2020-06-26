99bec7c562f8c955a3e58e8183aede63eaee96cc
Jeremy Winn <jeremyjwinn@gmail.com>
2020-06-26 21:17:38 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -17,11 +17,11 @@ def get_english_meaning(file_path, emoticon)
   data = load_library(file_path)
   english_meaing = ""
   data.each_with_object({}) do |(key,value)|
-    if key
-    
-    
-    
+    if value[1] == emoticon
+      english_meaing = value[0]
+    end
   end 
+  english_meaing
 end
 
 def get_japanese_emoticon
