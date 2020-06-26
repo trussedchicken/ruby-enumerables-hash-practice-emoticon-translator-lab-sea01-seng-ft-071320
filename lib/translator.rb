66745523419bb5737912538097c8c0b090a0a786
Jeremy Winn <jeremyjwinn@gmail.com>
2020-06-26 20:58:32 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -1,13 +1,28 @@
-# require modules here
+require 'pry'
+require "yaml"
 
-def load_library
-  # code goes here
+def load_library(file_path)
+    data = YAML.load_file(file_path)
+    result = data.each_with_object({}) do |(key,value), final_hash|
+      if !final_hash[key]
+        final_hash[key] = {
+          :english => value[0],
+          :japanese => value[1]
+        }
+      end 
+      result
+    end 
+end
+
+
+def get_english_meaning(file_path, emoticon)
+  data = load_library(file_path)
+  result = data.each_with_object({}) do |(key,value), final_meaning|
+    
+  end 
 end
 
 def get_japanese_emoticon
-  # code goes here
+
 end
 
-def get_english_meaning
-  # code goes here
-end
