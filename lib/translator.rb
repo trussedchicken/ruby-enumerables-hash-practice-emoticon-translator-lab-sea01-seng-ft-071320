61935affd34cafeaf2feede6b7a7d4c008beeb4a
Jeremy Winn <jeremyjwinn@gmail.com>
2020-06-26 21:03:47 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -4,14 +4,11 @@ require "yaml"
 def load_library(file_path)
     data = YAML.load_file(file_path)
     result = data.each_with_object({}) do |(key,value), final_hash|
-     # if !final_hash[key]
-      #  final_hash[key] = {
-       #   :english => value[0],
-        #  :japanese => value[1]
-        #}
-        binding.pry
-      end 
-      result
+      final_hash[key] = {
+        :english => value[0],
+        :japanese => value[1]
+        }
+    
     end 
 end
 
